101302f4c00d8db858f9919a36eb99fba61c60be
mariaruffo <maria.ruffo483@gmail.com>
2026-06-29 16:04:07 +0200

    modifica in prenotazioni

--- backend/prenotazioni.php
+++ backend/prenotazioni.php
@@ -5,7 +5,7 @@ require('database.php');
 header('Access-Control-Allow-Origin: *');
 
 $database = connectDatabase();
-$results  = $database->query('SELECT prenotazioni.*, sale.nome AS sala_nome FROM prenotazioni, sale WHERE prenotazioni.sala_id = sale.id ORDER BY prenotazioni.data_prenotazione ASC, prenotazioni.ora_inizio ASC');
+$results  = $database->query('SELECT prenotazioni.*, sale.nome AS sala_nome FROM prenotazioni JOIN sale ON prenotazioni.sala_id = sale.id ORDER BY prenotazioni.data_prenotazione ASC, prenotazioni.ora_inizio ASC');
 $rows     = $results->fetchAll(PDO::FETCH_ASSOC);
 
 $reservations = [];
